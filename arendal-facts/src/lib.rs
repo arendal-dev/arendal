dbe7740f9dfff8b96304b35f7438a480190c1373
Andres Rodriguez <andresrc@gmail.com>
2022-07-22 19:29:39 +0200

    Add Proof type

--- arendal-facts/src/lib.rs
+++ arendal-facts/src/lib.rs
@@ -7,6 +7,29 @@ use std::cmp::Ordering;
 use std::rc::Rc;
 
 #[derive(PartialEq, Debug)]
+enum Proof<T> {
+    Proved,
+    CannotProve,
+    Contradiction(T),
+}
+
+use self::Proof::*;
+
+impl<T: Eq> Proof<T> {
+    fn is_proved(&self) -> bool {
+        *self == Proved
+    }
+
+    fn map<U, F: FnOnce(T) -> U>(self, f: F) -> Proof<U> {
+        match self {
+            Proved => Proved,
+            CannotProve => CannotProve,
+            Contradiction(t) => Contradiction(f(t)),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 enum CombinationError {
     Incompatible,
 }
