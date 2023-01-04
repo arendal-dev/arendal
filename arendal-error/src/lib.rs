a704e1f1e7ce2987266bb0e6f7d22d383e66e1f6
Andres Rodriguez <andresrc@gmail.com>
2023-01-04 15:35:12 +0100

    More refactoring before starting with the parser

--- arendal-error/src/lib.rs
+++ arendal-error/src/lib.rs
@@ -14,38 +14,63 @@ pub fn error<T : errors::Error + 'static>(err : T) -> Error {
     Single(Box::new(err))
 }
 
-pub fn merge(e1 : Error, e2 : Error) -> Error {
-    match e1 {
-        Single(i1) => match e2 {
-            Single(i2) => Multiple(vec![i1, i2]),
-            Multiple(mut v2) => {
-                v2.insert(0, i1);
-                Multiple(v2)
-            }
-        },
-        Multiple(mut v1) => match e2 {
-            Single(i2) => {
-                v1.push(i2);
-                Multiple(v1)
-            }
-            Multiple(mut v2) => {
-                v1.append(&mut v2);
-                Multiple(v1)
+impl Error {
+    pub fn merge(self, error : Error) -> Error {
+        match self {
+            Single(i1) => match error {
+                Single(i2) => Multiple(vec![i1, i2]),
+                Multiple(mut v2) => {
+                    v2.insert(0, i1);
+                    Multiple(v2)
+                }
+            },
+            Multiple(mut v1) => match error {
+                Single(i2) => {
+                    v1.push(i2);
+                    Multiple(v1)
+                }
+                Multiple(mut v2) => {
+                    v1.append(&mut v2);
+                    Multiple(v1)
+                }
             }
         }
     }
 }
 
-pub fn merge_options(o1 : Option<Error>, o2 : Option<Error>) -> Option<Error> {
-    match o1 {
-        Some(e1) => match o2 {
-            Some(e2) => Some(merge(e1, e2)),
-            None => Some(e1)
-        },
-        None => o2,
-    }
+pub struct ErrorCollector {
+    error : Option<Error>
 }
 
+impl ErrorCollector {
+    pub fn new() -> ErrorCollector {
+        ErrorCollector { error: None }
+    }
+
+    pub fn add(&mut self,  error : Error) {
+        match &mut self.error {
+            None => self.error = Some(error),
+            _ => {
+                let e = self.error.take().unwrap();
+                self.error = Some(e.merge(error));
+            },
+        }
+    }
+
+    pub fn add_option(&mut self,  error : Option<Error>) {
+        match error {
+            Some(e) => self.add(e),
+            None => (),
+        }
+    }
+
+    pub fn to_result<T>(self, value : T) -> Result<T> {
+        match self.error {
+            None => Ok(value),
+            Some(e) => Err(e),
+        }
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
