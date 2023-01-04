bc6c642540cb97600974ac473b24e5aae47258e4
Andres Rodriguez <andresrc@gmail.com>
2023-01-04 15:35:12 +0100

    Move Indentation to parent module

--- arendal-parser/src/scanner/mod.rs
+++ arendal-parser/src/scanner/mod.rs
@@ -1,6 +1,7 @@
 mod errors;
 
 use arendal_error::{ErrorCollector, Result};
+use super::Indentation;
 use errors::*;
 
 pub fn scan(input: &str) -> Result<Vec<Token>> {
@@ -14,39 +15,6 @@ pub fn scan(input: &str) -> Result<Vec<Token>> {
     scanner.errors.to_result(scanner.tokens)
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Indentation {
-    tabs: usize,
-    spaces: usize,
-}
-
-impl Indentation {
-    #[inline]
-    fn new(tabs : usize, spaces: usize) -> Indentation {
-        Indentation { tabs, spaces }
-    }
-
-    fn get(input: &str) -> (Indentation, bool) {
-        let mut tabs : usize = 0;
-        let mut spaces : usize = 0;
-        let mut ok : bool = true;
-        for (i, c) in input.char_indices() {
-            if c == '\t' {
-                if spaces > 0 {
-                    ok = false;
-                    break;
-                }
-                tabs += 1;
-            } else if c == ' ' {
-                spaces += 1;
-            } else {
-                break;
-            }
-        }
-        ( Self::new(tabs, spaces), ok )
-    }
-    
-}
 
 
 #[derive(Debug)]
@@ -76,21 +44,3 @@ impl<'a> Scanner<'a> {
 
 }
 
-#[cfg(test)]
-mod tests {
-
-    use super::Indentation;
-
-    fn test_indentation(input: &str, tabs: usize, spaces: usize, ok: bool) {
-        assert_eq!(Indentation::get(input), (Indentation::new(tabs, spaces), ok));
-    }
-
-    #[test]
-    fn indentation() {
-        test_indentation("1", 0, 0, true);
-        test_indentation("\t1", 1, 0, true);
-        test_indentation("\t\t1", 2, 0, true);
-        test_indentation("\t\t 1", 2, 1, true);
-        test_indentation("\t\t  1", 2, 2, true);
-    }
-}
