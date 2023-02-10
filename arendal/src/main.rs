f6531ed706fbb23cd9b56e5582bc3967aa82d57c
Andres Rodriguez <andresrc@gmail.com>
2023-02-10 19:28:30 +0100

    Bootstrap CLI

---
+++ arendal/src/main.rs
@@ -0,0 +1,20 @@
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = Editor::<()>::new()?;
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
+}
