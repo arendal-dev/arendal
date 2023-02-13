5520bba96e9f23677e34bbea279ed8ef6c26faa4
Andres Rodriguez <andresrc@gmail.com>
2023-02-13 19:42:04 +0100

    Some test helpers

--- arendal-ast/src/bare.rs
+++ arendal-ast/src/bare.rs
@@ -11,10 +11,22 @@ pub fn lit_integer(value: Integer) -> Expression {
     super::Expression::lit_integer(BareLoc {}, value)
 }
 
+pub fn lit_i64(value: i64) -> Expression {
+    lit_integer(value.into())
+}
+
 pub fn unary(op: UnaryOp, expr: Expression) -> Expression {
     super::Expression::unary(BareLoc {}, op, expr)
 }
 
 pub fn binary(op: BinaryOp, expr1: Expression, expr2: Expression) -> Expression {
     super::Expression::binary(BareLoc {}, op, expr1, expr2)
+}
+
+pub fn add(left: Expression, right: Expression) -> Expression {
+    binary(BinaryOp::Add, left, right)
+}
+
+pub fn add_i64(v1: i64, v2: i64) -> Expression {
+    add(lit_i64(v1), lit_i64(v2))
 }
--- arendal-parser/src/parser/tests.rs
+++ arendal-parser/src/parser/tests.rs
@@ -1,45 +1,33 @@
 use super::parse_expression;
 
-use ast::bare::Expression;
-use ast::{bare, BinaryOp};
+use ast::bare::*;
+use ast::BinaryOp;
 
 fn check_expression(input: &str, expr: Expression) {
     assert_eq!(parse_expression(input).unwrap().to_bare(), expr);
 }
 
-fn int_literal(value: i64) -> Expression {
-    bare::lit_integer(value.into())
-}
-
-fn add(left: Expression, right: Expression) -> Expression {
-    bare::binary(BinaryOp::Add, left, right)
-}
-
-fn add_int(left: i64, right: i64) -> Expression {
-    add(int_literal(left), int_literal(right))
-}
-
 #[test]
 fn int_literal_expr() {
-    check_expression("1234", int_literal(1234));
+    check_expression("1234", lit_i64(1234));
 }
 
 #[test]
 fn sum1() {
-    check_expression("1+2", add_int(1, 2));
+    check_expression("1+2", add_i64(1, 2));
 }
 
 #[test]
 fn sum2() {
-    check_expression("1 + 2", add_int(1, 2));
+    check_expression("1 + 2", add_i64(1, 2));
 }
 
 #[test]
 fn sum3() {
-    check_expression("\t1 + 2", add_int(1, 2));
+    check_expression("\t1 + 2", add_i64(1, 2));
 }
 
 #[test]
 fn sum4() {
-    check_expression("1 + 2 + 3", add(add_int(1, 2), int_literal(3)));
+    check_expression("1 + 2 + 3", add(add_i64(1, 2), lit_i64(3)));
 }
