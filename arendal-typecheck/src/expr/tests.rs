39ee6963d8135f33dab457de50efeba3eae63b15
Andres Rodriguez <andresrc@gmail.com>
2023-02-14 07:03:49 +0100

    Use the test helpers

--- arendal-typecheck/src/expr/tests.rs
+++ arendal-typecheck/src/expr/tests.rs
@@ -1,15 +1,7 @@
 use super::check;
-use ast::bare::Expression;
+use ast::bare::*;
 use ast::error::Result;
 use ast::{Loc, Type, TypedExpression};
-
-fn int(value: i64) -> Expression {
-    ast::bare::lit_integer(value.into())
-}
-
-fn add_int(v1: i64, v2: i64) -> Expression {
-    ast::bare::binary(ast::BinaryOp::Add, int(v1), int(v2))
-}
 
 fn ok_type<L: Loc>(typed: Result<TypedExpression<L>>, t: Type) {
     assert_eq!(typed.unwrap().payload.loc_type, t);
@@ -17,10 +9,10 @@ fn ok_type<L: Loc>(typed: Result<TypedExpression<L>>, t: Type) {
 
 #[test]
 fn integer() {
-    ok_type(check(&int(1234)), Type::Integer);
+    ok_type(check(&lit_i64(1234)), Type::Integer);
 }
 
 #[test]
 fn sum() {
-    ok_type(check(&add_int(1, 2)), Type::Integer);
+    ok_type(check(&add_i64(1, 2)), Type::Integer);
 }
