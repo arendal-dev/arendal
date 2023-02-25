b75bdbcca8671ed7239747b120b6d01b99cef379
Andres Rodriguez <andresrc@gmail.com>
2023-02-25 10:46:34 +0100

    Type refs

--- arendal-ast/src/types.rs
+++ arendal-ast/src/types.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::rc::Rc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Type {
@@ -6,10 +7,53 @@ pub enum Type {
     True,
     False,
     Integer,
+    None,
+    Some(TypeRef),
+}
+
+impl Type {
+    pub fn to_ref(self) -> TypeRef {
+        TypeRef::new(self)
+    }
 }
 
 impl fmt::Display for Type {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct TypeRef {
+    inner: Rc<Type>,
+}
+
+impl TypeRef {
+    fn new(tipo: Type) -> Self {
+        TypeRef {
+            inner: Rc::new(tipo),
+        }
+    }
+}
+
+impl fmt::Display for TypeRef {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.inner.as_ref())
+    }
+}
+
+impl fmt::Debug for TypeRef {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.inner.as_ref())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Type, TypeRef};
+    #[test]
+    fn eq() {
+        let r = Type::Integer.to_ref();
+        assert_eq!(r, r.clone());
     }
 }
