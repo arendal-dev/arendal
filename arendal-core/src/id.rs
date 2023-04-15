1ffb204560fa9e290f4d2a115af64fd3d696f44b
Andres Rodriguez <andresrc@gmail.com>
2023-04-15 09:11:29 +0200

    Wrap Id in Arc

--- arendal-core/src/id.rs
+++ arendal-core/src/id.rs
@@ -1,13 +1,16 @@
 use crate::ArcStr;
-use std::fmt::{self, Write};
+use std::{
+    fmt::{self, Write},
+    sync::Arc,
+};
 
 // We use SHA3-256 hashes as ids when using content-adressing
 #[derive(Clone, PartialEq, Eq, Hash)]
-pub struct Id {
+pub struct RawId {
     bytes: [u8; 32],
 }
 
-impl Id {
+impl RawId {
     pub fn as_string(&self) -> String {
         data_encoding::HEXLOWER.encode(&self.bytes)
     }
@@ -17,13 +20,13 @@ impl Id {
     }
 }
 
-impl fmt::Display for Id {
+impl fmt::Display for RawId {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(self.as_string().as_str())
     }
 }
 
-impl fmt::Debug for Id {
+impl fmt::Debug for RawId {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str("Id(")?;
         fmt::Display::fmt(self, f)?;
@@ -31,13 +34,40 @@ impl fmt::Debug for Id {
     }
 }
 
-impl From<u64> for Id {
+impl From<u64> for RawId {
     fn from(value: u64) -> Self {
         let mut bytes = [0u8; 32];
         let le = value.to_le_bytes();
         for i in 0..le.len() {
             bytes[i] = le[i];
         }
-        Id { bytes }
+        RawId { bytes }
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct Id {
+    id: Arc<RawId>,
+}
+
+impl Id {
+    pub fn as_string(&self) -> String {
+        self.id.as_string()
+    }
+
+    pub fn as_arcstr(&self) -> ArcStr {
+        self.id.as_arcstr()
+    }
+}
+
+impl fmt::Display for Id {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.id.fmt(f)
+    }
+}
+
+impl fmt::Debug for Id {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.id.fmt(f)
     }
 }
