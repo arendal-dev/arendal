b64ca033cbfc24d5213b6ad3313a5595a923b66c
Andres Rodriguez <andresrc@gmail.com>
2023-04-30 07:35:56 +0200

    Change interpreter tests to use parsing

--- arendal-core/src/env/twi/tests.rs
+++ arendal-core/src/env/twi/tests.rs
@@ -1,44 +1,40 @@
 use crate::env::Env;
-use crate::error::Loc;
+use crate::error::Result;
 use crate::symbol::Pkg;
-use crate::typed::{ExprBuilder, Expression, Expressions, Module};
 
 use super::Value;
 
-const B: ExprBuilder = ExprBuilder::new(Loc::none());
-
-fn eval_ok(input: Expression, result: Value) {
+fn eval_module(input: &str) -> Result<Value> {
+    let parsed = crate::parser::parse(input)?;
     let mut env = Env::default();
-    let output = super::interpret(
-        &mut env,
-        &Module {
-            path: Pkg::Local.empty(),
-            expressions: Expressions::new(vec![input]),
-        },
-    );
-    if let Ok(v) = output {
-        assert_eq!(v, result);
-    } else {
-        panic!("Error evaluating expression");
+    let path = Pkg::Local.empty();
+    let module = crate::env::typecheck::check(&env, &path, &parsed)?;
+    super::interpret(&mut env, &module)
+}
+
+fn eval_ok(input: &str, result: Value) {
+    match eval_module(input) {
+        Ok(value) => assert_eq!(value, result),
+        Err(e) => panic!("Error evaluating expression {:?}", e),
     }
 }
 
 #[test]
 fn integer() {
-    eval_ok(B.val_i64(1234), Value::int64(1234));
+    eval_ok("1234", Value::int64(1234));
 }
 
 #[test]
 fn add1() {
-    eval_ok(B.add_i64(1, 2), Value::int64(3));
+    eval_ok("1+2", Value::int64(3));
 }
 
 #[test]
 fn add2() {
-    eval_ok(B.add(B.val_i64(3), B.add_i64(1, 2)), Value::int64(6));
+    eval_ok("3+1+2", Value::int64(6));
 }
 
 #[test]
 fn add_sub() {
-    eval_ok(B.sub(B.val_i64(3), B.add_i64(1, 2)), Value::int64(0));
+    eval_ok("3-(1+2)", Value::int64(0));
 }
