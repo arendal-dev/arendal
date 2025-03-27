cf9743fc12955e701b7aa74172b8aed976b4f9a6
Andres Rodriguez <andresrc@gmail.com>
2025-03-27 13:23:51 +0100

    Some methods for vectree

--- arendal-ast/src/vectree/mod.rs
+++ arendal-ast/src/vectree/mod.rs
@@ -1,62 +1,78 @@
 use std::cmp::{Eq, PartialEq};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct NodeRef {
-    tree: i64,
+pub struct NodeId {
+    tree_id: i64,
     index: usize,
 }
 
 pub trait Node {
-    fn get_children(&self, children: &mut Vec<NodeRef>);
+    fn get_children(&self, children: &mut Vec<NodeId>);
 }
 
 struct InternalNode<T: Node> {
-    id: NodeRef,
+    id: NodeId,
     node: T,
-    parent: Option<NodeRef>,
+    parent: Option<NodeId>,
 }
 
 type NodeVec<T> = Vec<InternalNode<T>>;
 
 pub struct VecTree<T: Node> {
+    tree_id: i64,
     nodes: NodeVec<T>,
-    top_level: Vec<NodeRef>,
+    top_level: Vec<NodeId>,
 }
 
-pub struct TopLevelIter<'a, T: Node> {
-    tree: &'a VecTree<T>,
-    iter: std::slice::Iter<'a, NodeRef>,
-}
+impl<T: Node> VecTree<T> {
+    pub fn get(&self, id: &NodeId) -> Option<&T> {
+        if self.tree_id != id.tree_id {
+            panic!("Wrong tree")
+        }
+        self.nodes.get(id.index).map(|n| &n.node)
+    }
 
-impl<'a, T: Node> Iterator for TopLevelIter<'a, T> {
-    type Item = (NodeRef, &'a T);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.iter.next() {
-            Some(id) => Some((id.clone(), &self.tree.nodes[id.index].node)),
-            None => None,
+    pub fn top_level_iter(&self) -> TopLevelIter<'_, T> {
+        TopLevelIter {
+            tree: self,
+            iter: self.top_level.iter(),
         }
     }
 }
 
+pub struct TopLevelIter<'a, T: Node> {
+    tree: &'a VecTree<T>,
+    iter: std::slice::Iter<'a, NodeId>,
+}
+
+impl<'a, T: Node> Iterator for TopLevelIter<'a, T> {
+    type Item = (NodeId, &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter
+            .next()
+            .map(|id| (id.clone(), &self.tree.nodes[id.index].node))
+    }
+}
+
 pub enum Error {
-    ChildNotFound(NodeRef),
-    ChildHasParent(NodeRef),
+    ChildNotFound(NodeId),
+    ChildHasParent(NodeId),
 }
 
 pub struct Builder<T: Node> {
-    tree: i64,
+    tree_id: i64,
     nodes: NodeVec<T>,
-    children: Vec<NodeRef>,
+    children: Vec<NodeId>,
 }
 
 impl<T: Node> Builder<T> {
-    pub fn add(&mut self, node: T) -> Result<NodeRef, Error> {
+    pub fn add(&mut self, node: T) -> Result<NodeId, Error> {
         self.children.clear();
         node.get_children(&mut self.children);
         // First pass: Validations
         for n in &self.children {
-            if n.tree != self.tree {
+            if n.tree_id != self.tree_id {
                 panic!("Invalid tree!")
             }
             let index = n.index;
@@ -68,8 +84,8 @@ impl<T: Node> Builder<T> {
                 return Err(Error::ChildHasParent(n.clone()));
             }
         }
-        let id = NodeRef {
-            tree: self.tree,
+        let id = NodeId {
+            tree_id: self.tree_id,
             index: self.nodes.len(),
         };
         // First pass: Update children
@@ -86,13 +102,14 @@ impl<T: Node> Builder<T> {
     }
 
     pub fn build(self) -> VecTree<T> {
-        let mut top_level = Vec::<NodeRef>::default();
+        let mut top_level = Vec::<NodeId>::default();
         for node in &self.nodes {
             if node.parent.is_none() {
                 top_level.push(node.id.clone());
             }
         }
         VecTree {
+            tree_id: self.tree_id,
             nodes: self.nodes,
             top_level,
         }
@@ -101,7 +118,7 @@ impl<T: Node> Builder<T> {
 
 pub fn builder<T: Node>() -> Builder<T> {
     Builder {
-        tree: rand::random(),
+        tree_id: rand::random(),
         nodes: Vec::default(),
         children: Vec::default(),
     }
