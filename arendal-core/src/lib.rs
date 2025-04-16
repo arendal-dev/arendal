2a2d4cf29a074c7a04f8c47c5db4a0b584e200c9
Andres Rodriguez <andresrc@gmail.com>
2025-04-16 08:56:33 +0200

    Clean up things moved to other crates

--- arendal-core/src/lib.rs
+++ arendal-core/src/lib.rs
@@ -1,15 +1,11 @@
-pub mod ast;
 pub mod ast0;
 pub mod context;
 pub mod env;
 pub mod error;
 pub mod id;
-pub mod input;
 pub mod keyword;
-pub mod problem;
 pub mod symbol;
 pub mod tst;
-pub mod vectree;
 pub mod visibility;
 
 use ::ast::{AST, problem::Result, stmt::Statement};
